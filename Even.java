293f2fdc113afcdf4c42cccf401e9f86b6fab3b5
BhuvaneshwariRamanathan <ramansenthil3@gmail.com>
2016-07-21 17:08:06 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,17 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Even {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+if(n%2==0){
+	System.out.println("The number is even");
+}
+else{
+	System.out.println("The number is odd");
+}
+}
+}

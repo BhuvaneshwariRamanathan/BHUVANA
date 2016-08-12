5b5eb2bc4617440b6f0ceab6caac28510175627d
BhuvaneshwariRamanathan <ramansenthil3@gmail.com>
2016-08-12 14:39:52 +0530

    Add files via upload

--- Swap.java
+++ Swap.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
 
 public class Swap {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Scanner s=new Scanner(System.in);
+System.out.println("Enter the to be swaped");
 int a=s.nextInt();
 int b=s.nextInt();
-int t;
+int t=0;
 t=b;
 b=a;
 a=t;
-System.out.println("After swap " +a +" "+b);
+System.out.println(a);
+System.out.println(b);
 	}
 
 }

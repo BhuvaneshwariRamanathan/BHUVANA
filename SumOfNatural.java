0dc7689bf4af2a6e60e5f961c3a47e9f7405b25e
BhuvaneshwariRamanathan <ramansenthil3@gmail.com>
2016-08-21 15:51:15 +0530

    Add files via upload

---
+++ SumOfNatural.java
@@ -0,0 +1,19 @@
+package guvi2;
+
+import java.util.Scanner;
+
+public class SumOfNatural {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the number");
+int n=s.nextInt();
+int sum=0;
+for(int i=0;i<=n;i++){
+	sum=sum+n;
+}
+System.out.println(sum);
+	}
+
+}

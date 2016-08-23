5f31b58d108eb1a2560fba72ce3ca10172b20d28
BhuvaneshwariRamanathan <ramansenthil3@gmail.com>
2016-08-23 17:53:33 +0530

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,13 @@
+package guvi.beginner;
+
+public class Ascii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=0;i<=255;i++){
+	char a=(char) i;
+	System.out.println(i+" "+a);
+}
+	}
+
+}
